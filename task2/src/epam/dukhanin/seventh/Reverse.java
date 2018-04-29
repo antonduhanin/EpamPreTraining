63e08cba25188a3124a1f4b524eb8a40f6ab824d
anton.duhanin_bank <antonduhanin4444@gmail.com>
2018-04-29 16:03:47 +0300

    modified reverse

--- task2/src/epam/dukhanin/seventh/Reverse.java
+++ task2/src/epam/dukhanin/seventh/Reverse.java
@@ -2,8 +2,12 @@ package epam.dukhanin.seventh;
 
 public class Reverse {
     public static int reverseNumber(int a) {
-        StringBuilder s = new StringBuilder(Integer.toString(a));
-        s.reverse();
-        return Integer.parseInt(new String(s));
+        int reversedNumber = 0;
+        while (a > 0) {
+            reversedNumber = reversedNumber * 10 + a % 10;
+            a = a / 10;
+        }
+
+        return reversedNumber;
     }
 }
